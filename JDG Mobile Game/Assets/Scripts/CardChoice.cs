5ef34c982a3735aeeebcd41c9995036a1eebd05f
Paul-Louis <paul-louis.renard@outlook.fr>
2021-02-16 18:43:44 +0100

    Random deck

--- JDG Mobile Game/Assets/Scripts/CardChoice.cs
+++ JDG Mobile Game/Assets/Scripts/CardChoice.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = System.Random;
 
 public class CardChoice : MonoBehaviour
 {
@@ -101,8 +104,8 @@ public class CardChoice : MonoBehaviour
                 SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
                 label.text = "Choix de cartes pour le joueur 1";
                 buttonLabel.text = "Choix joueur 2";
-                isPlayerOneCardChosen = true;
-                gameState.GetComponent<GameState>().DeckP1 = deck;
+                isPlayerOneCardChosen = false;
+                gameState.GetComponent<GameState>().DeckP2 = deck;
             }
             else
             {
@@ -110,6 +113,40 @@ public class CardChoice : MonoBehaviour
                 displayMessageBox(remainedCards);
             }
         }
+    }
+
+    public void getRandomDeck()
+    {
+        List<Card> deck1 = new List<Card>();
+        List<Card> deck2 = new List<Card>();
+
+        int size = gameState.GetComponent<GameState>().allCards.Count;
+        Card[] cards = new Card[size];
+
+        gameState.GetComponent<GameState>().allCards.CopyTo(cards);
+
+        List<Card> allDeck = cards.ToList();
+        Random alea = new Random();
+
+        for (int i = 0; i < 30; i++)
+        {
+            int random = alea.Next(size);
+            deck1.Add(allDeck[random]);
+            allDeck.Remove(allDeck[random]);
+            size--;
+            random = alea.Next(size);
+            deck2.Add(allDeck[random]);
+            allDeck.Remove(allDeck[random]);
+            size--;
+        }
+
+        gameState.GetComponent<GameState>().DeckP1 = deck1;
+        gameState.GetComponent<GameState>().DeckP2 = deck2;
+        
+        SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
+        label.text = "Choix de cartes pour le joueur 1";
+        buttonLabel.text = "Choix joueur 2";
+        isPlayerOneCardChosen = true;
     }
 
     public void back()
