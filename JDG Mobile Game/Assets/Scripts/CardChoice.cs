2da0c6f061cd88b743d61e5a742a2cd1df5d7119
Paul-Louis <paul-louis.renard@outlook.fr>
2021-12-04 15:12:25 +0100

    Moïse condition

--- JDG Mobile Game/Assets/Scripts/CardChoice.cs
+++ JDG Mobile Game/Assets/Scripts/CardChoice.cs
@@ -119,8 +119,8 @@ public class CardChoice : MonoBehaviour
         deck1.Add(GetSpecificCard("Joueur Du Grenier", allCards));
         deck1.Add(GetSpecificCard("La Petite Fille", allCards));
         deck1.Add(GetSpecificCard("Le mot de passe", allCards));
-        deck1.Add(GetSpecificCard("Mecha-Granolax", allCards));
-        deck1.Add(GetSpecificCard("Granolax", allCards));
+        deck1.Add(GetSpecificCard("Papy Grenier", allCards));
+        deck1.Add(GetSpecificCard("Moïse, le plus grand de tous les hébreux", allCards));
         
         while (deck1.Count != 30)
         {
