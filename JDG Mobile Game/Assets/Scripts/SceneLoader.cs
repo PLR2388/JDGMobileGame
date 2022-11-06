c58dd44b7359c26573db729481a6c49d438b04cd
Paul-Louis <paul-louis.renard@outlook.fr>
2022-11-06 12:09:33 +0100

    Add toast

--- JDG Mobile Game/Assets/Scripts/SceneLoader.cs
+++ JDG Mobile Game/Assets/Scripts/SceneLoader.cs
@@ -7,4 +7,35 @@ public class SceneLoader : MonoBehaviour
         Application.Quit();
         Debug.Log("Quit!");
     }
+
+    public void OnClickTuto()
+    {
+        _ShowAndroidToastMessage("Le tutoriel est en cours de développement ...");
+    }
+
+    public void onClickStory()
+    {
+        _ShowAndroidToastMessage("Si tu as des idées pour l'histoire, je t'invite à me contacter !");
+    }
+    
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+#if UNITY_EDITOR
+        Debug.Log(message);
+#elif UNITY_ANDROID
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+#endif
+    }
 }
