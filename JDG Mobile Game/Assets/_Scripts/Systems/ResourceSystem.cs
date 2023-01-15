7e0682273ed7e582c87c43ce46e63153f4083216
Paul-Louis <paul-louis.renard@outlook.fr>
2023-01-15 19:05:37 +0100

    Add getter

--- JDG Mobile Game/Assets/_Scripts/Systems/ResourceSystem.cs
+++ JDG Mobile Game/Assets/_Scripts/Systems/ResourceSystem.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class ResourceSystem : StaticInstance<ResourceSystem>
 {
     public List<Card> Cards { get; private set; }
+    private Dictionary<string, Card> _CardsDict;
 
     protected override void Awake()
     {
@@ -17,5 +18,8 @@ public class ResourceSystem : StaticInstance<ResourceSystem>
     private void AssembleResources()
     {
         Cards = Resources.LoadAll<Card>("Cards").ToList();
+        _CardsDict = Cards.ToDictionary(card => card.Title, card => card);
     }
+
+    public Card GetCardByName(string title) => _CardsDict[title];
 }
