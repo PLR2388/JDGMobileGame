28f04c9a65a961b6e8ac30f1888c9ce346c4a1a7
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-21 12:00:43 +0200

    EnumGenerator

--- JDG Mobile Game/Assets/_Scripts/Utilities/EnumGenerator.cs
+++ JDG Mobile Game/Assets/_Scripts/Utilities/EnumGenerator.cs
@@ -3,17 +3,25 @@ using System.Text;
 using UnityEngine;
 using Newtonsoft.Json.Linq;
 
+/// <summary>
+/// Responsible for generating enums and associated mappings from JSON localization data.
+/// </summary>
 public class EnumGenerator: MonoBehaviour
 {
-    StringBuilder enumBuilder = new StringBuilder();
-    StringBuilder mappingBuilder = new StringBuilder();
+    StringBuilder enumBuilder = new();
+    StringBuilder mappingBuilder = new();
 
-
+    /// <summary>
+    /// Called when the script instance is being loaded.
+    /// </summary>
     void Awake()
     {
         GenerateEnumFromJson();
     }
     
+    /// <summary>
+    /// Generates an enum and its associated mappings from the JSON localization data.
+    /// </summary>
     private void GenerateEnumFromJson()
     {
         TextAsset jsonFile = Resources.Load<TextAsset>("Localization/fr");
@@ -23,6 +31,11 @@ public class EnumGenerator: MonoBehaviour
         SaveToFile();
     }
 
+    /// <summary>
+    /// Recursively generates enum values and associated mappings for a given JSON object.
+    /// </summary>
+    /// <param name="jsonObject">The JSON object to parse.</param>
+    /// <param name="parentKey">The parent key (if any) for nested properties.</param>
     private void GenerateEnumAndMapping(JObject jsonObject, string parentKey = "")
     {
         foreach (var property in jsonObject.Properties())
@@ -41,6 +54,9 @@ public class EnumGenerator: MonoBehaviour
         }
     }
     
+    /// <summary>
+    /// Saves the generated enum and its associated mappings to their respective .cs files.
+    /// </summary>
     private void SaveToFile()
     {
         enumBuilder.Insert(0, "public enum LocalizationKeys\n{");
