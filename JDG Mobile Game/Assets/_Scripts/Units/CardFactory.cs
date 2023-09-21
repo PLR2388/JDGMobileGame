da3de0ad35ec047c232e2f0f2cf154672cb1ad53
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-21 13:31:25 +0200

    CardFactory

--- JDG Mobile Game/Assets/_Scripts/Units/CardFactory.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/CardFactory.cs
@@ -6,8 +6,18 @@ using Cards.EquipmentCards;
 using Cards.FieldCards;
 using Cards.InvocationCards;
 
+/// <summary>
+/// Provides functionality to create specific instances of InGameCard based on the provided card type.
+/// </summary>
 public class CardFactory
 {
+    /// <summary>
+    /// Creates an instance of InGameCard based on the type and owner of the provided card.
+    /// </summary>
+    /// <param name="card">The base card for which the InGameCard is to be created.</param>
+    /// <param name="cardOwner">The owner of the card.</param>
+    /// <returns>An instance of a specific InGameCard subtype based on the card provided.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when an unsupported card type is provided.</exception>
     public static InGameCard CreateInGameCard(Card card, CardOwner cardOwner)
     {
         return card switch
@@ -16,7 +26,7 @@ public class CardFactory
             EffectCard effectCard => InGameEffectCard.Init(effectCard, cardOwner),
             InvocationCard invocationCard => InGameInvocationCard.Init(invocationCard, cardOwner),
             FieldCard fieldCard => InGameFieldCard.Init(fieldCard, cardOwner),
-            _ => throw new InvalidOperationException()
+            _ => throw new InvalidOperationException("Unsupported card type provided.")
         };
     }
 }
