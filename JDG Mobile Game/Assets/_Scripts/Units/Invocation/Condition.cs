e649070fe1f36bd6e287ee55665838d5065470f4
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-22 10:06:12 +0200

    Condition

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Condition.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Condition.cs
@@ -1,3 +1,6 @@
+/// <summary>
+/// Enumerates the names of various conditions in the game.
+/// </summary>
 public enum ConditionName
 {
     BenzaieJeuneOrBenzaieOnField,
@@ -25,10 +28,25 @@ public enum ConditionName
     JapanOnField
 }
 
+/// <summary>
+/// Represents an abstract condition that can be evaluated against player cards.
+/// </summary>
 public abstract class Condition
 {
+    /// <summary>
+    /// Gets or sets the name of the condition.
+    /// </summary>
     public ConditionName Name { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the description of the condition.
+    /// </summary>
     protected string Description { get; set; }
 
+    /// <summary>
+    /// Determines whether the condition can be met with the given player cards.
+    /// </summary>
+    /// <param name="playerCards">The player cards to evaluate the condition against.</param>
+    /// <returns><c>true</c> if the condition can be met; otherwise, <c>false</c>.</returns>
     public abstract bool CanBeSummoned(PlayerCards playerCards);
 }
