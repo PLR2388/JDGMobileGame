d07ccd5827915b9b78280b6aa3f2a507a767bb64
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-28 18:52:49 +0200

    SkipOpponentAttackAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/SkipOpponentAttackAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/SkipOpponentAttackAbility.cs
@@ -3,15 +3,28 @@ using _Scripts.Units.Invocation;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability to skip the opponent's attack in the game.
+/// </summary>
 public class SkipOpponentAttackAbility : Ability
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SkipOpponentAttackAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
     public SkipOpponentAttackAbility(AbilityName name, string description)
     {
         Name = name;
         Description = description;
     }
 
-    protected static void DisplayOkMessage(Transform canvas, string message)
+    /// <summary>
+    /// Displays an OK message box with the specified message.
+    /// </summary>
+    /// <param name="canvas">The canvas to display the message box on.</param>
+    /// <param name="message">The message to display.</param>
+    private static void DisplayOkMessage(Transform canvas, string message)
     {
 
         var config = new MessageBoxConfig(
@@ -25,7 +38,12 @@ public class SkipOpponentAttackAbility : Ability
         MessageBox.Instance.CreateMessageBox(canvas, config);
     }
 
-    private void ApplyEffect(Transform canvas, PlayerCards opponentPlayerCard)
+    /// <summary>
+    /// Applies the effect of skipping the opponent's attack.
+    /// </summary>
+    /// <param name="canvas">The canvas to display UI elements on.</param>
+    /// <param name="opponentPlayerCard">The opponent's player cards.</param>
+    private static void ApplyEffect(Transform canvas, PlayerCards opponentPlayerCard)
     {
         if (opponentPlayerCard.InvocationCards.Count > 0)
         {
@@ -62,9 +80,6 @@ public class SkipOpponentAttackAbility : Ability
                                     LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.INFORMATION_NO_SKIP_ATTACK_MESSAGE)
                                 );
                             }
-                        },
-                        negativeAction: () =>
-                        {
                         }
                     );
                     CardSelector.Instance.CreateCardSelection(canvas, config);
@@ -74,12 +89,17 @@ public class SkipOpponentAttackAbility : Ability
         }
     }
 
-
+    /// <summary>
+    /// Overrides the base ApplyEffect method to apply this ability's effect.
+    /// </summary>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         ApplyEffect(canvas, opponentPlayerCards);
     }
 
+    /// <summary>
+    /// Handles the OnTurnStart event to apply the ability effect at the start of the turn.
+    /// </summary>
     public override void OnTurnStart(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         if (invocationCard.CancelEffect)
