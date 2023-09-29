cf04f9f396eefc3469b5fc5ad6cbd9985816a455
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-29 05:35:20 +0200

    LimitTurnExistenceAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/LimitTurnExistenceAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/LimitTurnExistenceAbility.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability that limits the number of turns an invocation card can exist on the field.
+/// </summary>
 public class LimitTurnExistenceAbility : Ability
 {
-    private int numberOfTurn;
+    private readonly int numberOfTurn;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LimitTurnExistenceAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="numberTurn">The number of turns the invocation card can exist on the field.</param>
     public LimitTurnExistenceAbility(AbilityName name, string description, int numberTurn)
     {
         Name = name;
@@ -11,6 +20,13 @@ public class LimitTurnExistenceAbility : Ability
         numberOfTurn = numberTurn;
     }
 
+    /// <summary>
+    /// Invoked at the start of each turn, this method checks the existence duration of the associated
+    /// invocation card and moves it to the yellow cards collection if the limit is reached.
+    /// </summary>
+    /// <param name="canvas">The canvas transform.</param>
+    /// <param name="playerCards">The player's cards.</param>
+    /// <param name="opponentPlayerCards">The opponent player's cards.</param>
     public override void OnTurnStart(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         if (invocationCard.CancelEffect)
