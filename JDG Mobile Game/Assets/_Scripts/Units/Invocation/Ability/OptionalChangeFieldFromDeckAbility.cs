84fac32400b622754e2ce04c77eda6b1236a4e2e
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-29 05:33:36 +0200

    OptionalChangeFieldFromDeckAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/OptionalChangeFieldFromDeckAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/OptionalChangeFieldFromDeckAbility.cs
@@ -2,27 +2,41 @@ using System.Collections.Generic;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability that allows for an optional change of the field card from the deck.
+/// </summary>
 public class OptionalChangeFieldFromDeckAbility : Ability
 {
+    /// <summary>
+    /// Represents an ability that allows for an optional change of the field card from the deck.
+    /// </summary>
     public OptionalChangeFieldFromDeckAbility(AbilityName name, string description)
     {
         Name = name;
         Description = description;
     }
 
+    /// <summary>
+    /// Displays an OK message box with a message indicating no field card is set.
+    /// </summary>
+    /// <param name="canvas">The game canvas.</param>
     private static void DisplayOkMessageBox(Transform canvas)
     {
         var config = new MessageBoxConfig(
             LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.INFORMATION_TITLE),
             LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.INFORMATION_NO_FIELD_CARD_SET_MESSAGE),
-            showOkButton: true,
-            okAction: () =>
-            {
-            }
+            showOkButton: true
         );
         MessageBox.Instance.CreateMessageBox(canvas, config);
     }
 
+    /// <summary>
+    /// Applies the effect of the ability, offering an option to change the field card from the deck,
+    /// if any field cards are available in the deck.
+    /// </summary>
+    /// <param name="canvas">The game canvas.</param>
+    /// <param name="playerCards">The cards of the current player.</param>
+    /// <param name="opponentPlayerCards">The cards of the opponent player.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         List<InGameCard> fieldCardsFromDeck = playerCards.Deck.FindAll(card => card.Type == CardType.Field);
