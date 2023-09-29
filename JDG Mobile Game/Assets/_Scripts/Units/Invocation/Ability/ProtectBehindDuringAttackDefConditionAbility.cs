bd586bec83ebdea9144cb7225167642e510785cc
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-29 05:20:50 +0200

    ProtectBehindDuringAttackDefConditionAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/ProtectBehindDuringAttackDefConditionAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/ProtectBehindDuringAttackDefConditionAbility.cs
@@ -3,15 +3,35 @@ using System.Linq;
 using _Scripts.Units.Invocation;
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability that protects a card positioned behind during an attack, 
+/// based on certain defensive conditions.
+/// </summary>
 public class ProtectBehindDuringAttackDefConditionAbility : Ability
 {
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProtectBehindDuringAttackDefConditionAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
     public ProtectBehindDuringAttackDefConditionAbility(AbilityName name, string description)
     {
         Name = name;
         Description = description;
     }
 
+    /// <summary>
+    /// Triggered when a card is attacked. Determines whether the ability should protect
+    /// another card positioned behind based on the defensive condition.
+    /// </summary>
+    /// <param name="canvas">The canvas to display any UI elements on.</param>
+    /// <param name="attackedCard">The card being attacked.</param>
+    /// <param name="attacker">The card initiating the attack.</param>
+    /// <param name="playerCards">The collection of cards of the player being attacked.</param>
+    /// <param name="opponentPlayerCards">The collection of cards of the attacking player.</param>
+    /// <param name="currentPlayerStatus">The status of the player being attacked.</param>
+    /// <param name="opponentPlayerStatus">The status of the attacking player.</param>
     public override void OnCardAttacked(Transform canvas, InGameInvocationCard attackedCard,
         InGameInvocationCard attacker, PlayerCards playerCards, PlayerCards opponentPlayerCards,
         PlayerStatus currentPlayerStatus, PlayerStatus opponentPlayerStatus)
@@ -35,7 +55,6 @@ public class ProtectBehindDuringAttackDefConditionAbility : Ability
             else
             {
                 // Random choice for the moment
-                // TODO Change
                 InGameInvocationCard newAttackedCard = inGameInvocationCards[Random.Range(0, inGameInvocationCards.Count)];
                 base.OnCardAttacked(canvas, newAttackedCard, attacker, playerCards, opponentPlayerCards,
                     currentPlayerStatus, opponentPlayerStatus);
