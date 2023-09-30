293db2e4738c816cd6b9a45ea85ea5b0848cd8e2
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 17:39:12 +0200

    DrawMoreCardsAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Field/FieldAbility/DrawMoreCardsAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Field/FieldAbility/DrawMoreCardsAbility.cs
@@ -1,11 +1,20 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// Represents a field ability that allows the player to draw more cards at the start of their turn.
+/// </summary>
 public class DrawMoreCardsAbility : FieldAbility
 {
 
-    private int numberAdditionalCards;
+    private readonly int numberAdditionalCards;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DrawMoreCardsAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the field ability.</param>
+    /// <param name="description">The description of the field ability.</param>
+    /// <param name="numberAdditionalCard">The number of additional cards to draw.</param>
     public DrawMoreCardsAbility(FieldAbilityName name, string description, int numberAdditionalCard)
     {
         Name = name;
@@ -13,6 +22,12 @@ public class DrawMoreCardsAbility : FieldAbility
         numberAdditionalCards = numberAdditionalCard;
     }
 
+    /// <summary>
+    /// Invoked at the start of the turn, allowing the player to draw additional cards if available in their deck.
+    /// </summary>
+    /// <param name="canvas">The UI canvas where gameplay elements are displayed.</param>
+    /// <param name="playerCards">The player's current set of cards.</param>
+    /// <param name="playerStatus">The current status of the player.</param>
     public override void OnTurnStart(Transform canvas,PlayerCards playerCards, PlayerStatus playerStatus)
     {
         base.OnTurnStart(canvas, playerCards, playerStatus);
