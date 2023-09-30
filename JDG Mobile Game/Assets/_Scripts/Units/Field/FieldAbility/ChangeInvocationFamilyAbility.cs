67a78a7d25a4b8da1f03b00a89464815c6bc1f3b
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 17:43:11 +0200

    ChangeInvocationFamilyAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Field/FieldAbility/ChangeInvocationFamilyAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Field/FieldAbility/ChangeInvocationFamilyAbility.cs
@@ -2,11 +2,21 @@ using System.Linq;
 using _Scripts.Units.Invocation;
 using Cards;
 
+/// <summary>
+/// Represents a field ability that can change the family of a specific invocation card.
+/// </summary>
 public class ChangeInvocationFamilyAbility : FieldAbility
 {
-    private string invocationName;
-    private CardFamily family;
+    private readonly string invocationName;
+    private readonly CardFamily family;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChangeInvocationFamilyAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the field ability.</param>
+    /// <param name="description">The description of the field ability.</param>
+    /// <param name="invocationName">The name of the invocation card to be affected.</param>
+    /// <param name="family">The card family to be set.</param>
     public ChangeInvocationFamilyAbility(FieldAbilityName name, string description, string invocationName,
         CardFamily family)
     {
@@ -16,6 +26,12 @@ public class ChangeInvocationFamilyAbility : FieldAbility
         this.family = family;
     }
 
+    /// <summary>
+    /// Notifies other field abilities about the change in family for an invocation card.
+    /// </summary>
+    /// <param name="playerCards">The player's cards context.</param>
+    /// <param name="previousFamilies">Previous families of the invocation card.</param>
+    /// <param name="invocationCard">The invocation card that has its family changed.</param>
     private static void NotifyChangeFamilyFieldCard(PlayerCards playerCards, CardFamily[] previousFamilies,
         InGameInvocationCard invocationCard)
     {
@@ -26,6 +42,10 @@ public class ChangeInvocationFamilyAbility : FieldAbility
         }
     }
 
+    /// <summary>
+    /// Applies the effect, changing the family of the specified invocation card.
+    /// </summary>
+    /// <param name="playerCards">The player's cards context.</param>
     public override void ApplyEffect(PlayerCards playerCards)
     {
         base.ApplyEffect(playerCards);
@@ -39,7 +59,11 @@ public class ChangeInvocationFamilyAbility : FieldAbility
         }
     }
 
-
+    /// <summary>
+    /// Handles the event when a new invocation card is added to the player's context.
+    /// </summary>
+    /// <param name="invocationCard">The added invocation card.</param>
+    /// <param name="playerCards">The player's cards context.</param>
     public override void OnInvocationCardAdded(InGameInvocationCard invocationCard, PlayerCards playerCards)
     {
         base.OnInvocationCardAdded(invocationCard, playerCards);
@@ -51,6 +75,10 @@ public class ChangeInvocationFamilyAbility : FieldAbility
         }
     }
 
+    /// <summary>
+    /// Handles the event when a field card is removed from the player's context.
+    /// </summary>
+    /// <param name="playerCards">The player's cards context.</param>
     public override void OnFieldCardRemoved(PlayerCards playerCards)
     {
         base.OnFieldCardRemoved(playerCards);
