34b2141723abe5b192bb786f0d98c430855ef6b9
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 17:44:50 +0200

    InGameEquipmentCard

--- JDG Mobile Game/Assets/_Scripts/Units/Equipment/InGameEquipmentCard.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Equipment/InGameEquipmentCard.cs
@@ -3,13 +3,23 @@ using System.Linq;
 using Cards;
 using Cards.EquipmentCards;
 
+/// <summary>
+/// Represents an in-game version of an equipment card with its abilities.
+/// </summary>
 public class InGameEquipmentCard : InGameCard
 {
-    private EquipmentCard baseEquipmentCard;
+    private readonly EquipmentCard baseEquipmentCard;
 
+    /// <summary>
+    /// List of abilities associated with the equipment card.
+    /// </summary>
     public List<EquipmentAbility> EquipmentAbilities = new List<EquipmentAbility>();
 
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InGameEquipmentCard"/> class.
+    /// </summary>
+    /// <param name="equipmentCard">The base equipment card this in-game card is based on.</param>
+    /// <param name="cardOwner">The owner of this card.</param>
     public InGameEquipmentCard(EquipmentCard equipmentCard, CardOwner cardOwner)
     {
         baseEquipmentCard = equipmentCard;
@@ -17,6 +27,9 @@ public class InGameEquipmentCard : InGameCard
         Reset();
     }
 
+    /// <summary>
+    /// Resets the in-game equipment card's details to match its base equipment card.
+    /// </summary>
     private void Reset()
     {
         title = baseEquipmentCard.Title;
