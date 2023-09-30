4c8930250eae3d7d6fb60b2173cc61c501521eef
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 14:36:44 +0200

    CopyAtkDefAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/CopyAtkDefAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/CopyAtkDefAbility.cs
@@ -3,10 +3,21 @@ using System.Linq;
 using _Scripts.Units.Invocation;
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability to copy the attack and defense stats of a specified card.
+/// If the target card (with stats to be copied) is present in the player's invocation cards, 
+/// the ability will replace the ability holder's attack and defense with the target's stats.
+/// </summary>
 public class CopyAtkDefAbility : Ability
 {
-    private string cardToCopyName;
+    private readonly string cardToCopyName;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CopyAtkDefAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="copiedCard">The name of the card whose stats are to be copied.</param>
     public CopyAtkDefAbility(AbilityName name, string description, string copiedCard)
     {
         Name = name;
@@ -14,6 +25,10 @@ public class CopyAtkDefAbility : Ability
         cardToCopyName = copiedCard;
     }
 
+    /// <summary>
+    /// Copies the attack and defense stats of the specified card if present.
+    /// </summary>
+    /// <param name="playerCards">The player's current set of cards.</param>
     private void ApplyCopy(PlayerCards playerCards)
     {
         try
@@ -25,29 +40,58 @@ public class CopyAtkDefAbility : Ability
         }
         catch (Exception e)
         {
-            // invocation is not present amongs invocationCards
+            // Desired card is not found among the invocation cards
             Console.WriteLine(e);
         }
     }
 
+    /// <summary>
+    /// Applies the effect to copy attack and defense stats.
+    /// </summary>
+    /// <param name="canvas">The UI canvas.</param>
+    /// <param name="playerCards">The player's collection of cards.</param>
+    /// <param name="opponentPlayerCards">The opponent's collection of cards.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         ApplyCopy(playerCards);
     }
 
+    /// <summary>
+    /// Resets the invocation card's attack and defense stats.
+    /// </summary>
+    /// <param name="playerCards">The player's collection of cards.</param>
     public override void CancelEffect(PlayerCards playerCards)
     {
         base.CancelEffect(playerCards);
+        ResetInvocationAttackDefense();
+    }
+    
+    /// <summary>
+    /// Resets the invocation card's attack and defense to base stats.
+    /// </summary>
+    private void ResetInvocationAttackDefense()
+    {
+
         invocationCard.Attack = invocationCard.BaseInvocationCard.BaseInvocationCardStats.Attack;
         invocationCard.Defense = invocationCard.BaseInvocationCard.BaseInvocationCardStats.Defense;
     }
 
+    /// <summary>
+    /// Reactivates the copying effect.
+    /// </summary>
+    /// <param name="playerCards">The player's collection of cards.</param>
     public override void ReactivateEffect(PlayerCards playerCards)
     {
         base.ReactivateEffect(playerCards);
         ApplyCopy(playerCards);
     }
 
+    /// <summary>
+    /// Applies the copying effect when the turn starts.
+    /// </summary>
+    /// <param name="canvas">The UI canvas.</param>
+    /// <param name="playerCards">The player's collection of cards.</param>
+    /// <param name="opponentPlayerCards">The opponent's collection of cards.</param>
     public override void OnTurnStart(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         if (invocationCard.CancelEffect)
@@ -58,6 +102,11 @@ public class CopyAtkDefAbility : Ability
         ApplyCopy(playerCards);
     }
 
+    /// <summary>
+    /// Reactivates the copy effect when a card is added.
+    /// </summary>
+    /// <param name="newCard">The new card added.</param>
+    /// <param name="playerCards">The player's collection of cards.</param>
     public override void OnCardAdded(InGameInvocationCard newCard, PlayerCards playerCards)
     {
         base.OnCardAdded(newCard, playerCards);
@@ -69,6 +118,11 @@ public class CopyAtkDefAbility : Ability
         ApplyCopy(playerCards);
     }
 
+    /// <summary>
+    /// Resets the invocation card stats if the specified card to copy is removed.
+    /// </summary>
+    /// <param name="removeCard">The card that was removed.</param>
+    /// <param name="playerCards">The player's collection of cards.</param>
     public override void OnCardRemove(InGameInvocationCard removeCard, PlayerCards playerCards)
     {
         base.OnCardRemove(removeCard, playerCards);
@@ -79,8 +133,7 @@ public class CopyAtkDefAbility : Ability
 
         if (removeCard.Title == cardToCopyName)
         {
-            invocationCard.Attack = invocationCard.BaseInvocationCard.BaseInvocationCardStats.Attack;
-            invocationCard.Defense = invocationCard.BaseInvocationCard.BaseInvocationCardStats.Defense;
+            ResetInvocationAttackDefense();
         }
     }
     
