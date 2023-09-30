e8b5b6f490478cbbd9f38f6b91df74db4afdf223
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 14:07:03 +0200

    GetTypeCardFromDeckWithoutAttackAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/GetTypeCardFromDeckWithoutAttackAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/GetTypeCardFromDeckWithoutAttackAbility.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class GetTypeCardFromDeckWithoutAttackAbility : Ability
 {
-    private CardType type;
+    private readonly CardType type;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetTypeCardFromDeckWithoutAttackAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="cardType">The type of card this ability is concerned with.</param>
     public GetTypeCardFromDeckWithoutAttackAbility(AbilityName name, string description, CardType cardType)
     {
         Name = name;
@@ -13,16 +19,17 @@ public class GetTypeCardFromDeckWithoutAttackAbility : Ability
         type = cardType;
     }
 
-    protected static void DisplayOkMessage(Transform canvas)
+    /// <summary>
+    /// Displays a message box with an OK button indicating no valid card was found.
+    /// </summary>
+    /// <param name="canvas">The game canvas where the message box will be displayed.</param>
+    private static void DisplayOkMessage(Transform canvas)
     {
 
         var config = new MessageBoxConfig(
             LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.INFORMATION_TITLE),
             LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.INFORMATION_NO_CARD_GET_FROM_DECK_MESSAGE),
-            showOkButton: true,
-            okAction: () =>
-            {
-            }
+            showOkButton: true
         );
         MessageBox.Instance.CreateMessageBox(
             canvas,
@@ -30,6 +37,13 @@ public class GetTypeCardFromDeckWithoutAttackAbility : Ability
         );
     }
 
+    /// <summary>
+    /// Applies the ability effect on the player's cards. Checks if there's a card in the player's deck that matches the specified type.
+    /// If found, prompts the user with an option to select it. If not, a message box is displayed.
+    /// </summary>
+    /// <param name="canvas">The game canvas where UI components will be displayed.</param>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="opponentPlayerCards">The opponent's current cards. (Not used in this method, but included due to override)</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         bool hasCardInDeck = playerCards.Deck.Exists(card => card.Type == type);
