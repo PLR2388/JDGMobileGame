e3b4a139f4cfde0b5ca0b1a0ae50386f609929fb
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 17:36:56 +0200

    EarnATKDEFForFamilyAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Field/FieldAbility/EarnATKDEFForFamilyAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Field/FieldAbility/EarnATKDEFForFamilyAbility.cs
@@ -3,12 +3,23 @@ using System.Linq;
 using _Scripts.Units.Invocation;
 using Cards;
 
+/// <summary>
+/// Represents a field ability that modifies the attack and defense of invocations based on their family.
+/// </summary>
 public class EarnATKDEFForFamilyAbility : FieldAbility
 {
-    private float bonusAttack;
-    private float bonusDefense;
-    private CardFamily family;
+    private readonly float bonusAttack;
+    private readonly float bonusDefense;
+    private readonly CardFamily family;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EarnATKDEFForFamilyAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the field ability.</param>
+    /// <param name="description">The description of the field ability.</param>
+    /// <param name="atk">Bonus attack to be applied.</param>
+    /// <param name="def">Bonus defense to be applied.</param>
+    /// <param name="family">The family of invocation cards this ability applies to.</param>
     public EarnATKDEFForFamilyAbility(FieldAbilityName name, string description, float atk, float def,
         CardFamily family)
     {
@@ -19,6 +30,11 @@ public class EarnATKDEFForFamilyAbility : FieldAbility
         this.family = family;
     }
 
+    /// <summary>
+    /// Builds a list of valid invocation cards based on the family.
+    /// </summary>
+    /// <param name="playerCards">The player's cards.</param>
+    /// <returns>A list of valid invocation cards.</returns>
     private List<InGameInvocationCard> BuildValidInvocationCards(PlayerCards playerCards)
     {
         var invocationsList = family == CardFamily.Any
@@ -27,36 +43,71 @@ public class EarnATKDEFForFamilyAbility : FieldAbility
         return invocationsList.ToList();
     }
 
+    /// <summary>
+    /// Applies the effect of this ability to the player's cards.
+    /// </summary>
+    /// <param name="playerCards">The player's cards.</param>
     public override void ApplyEffect(PlayerCards playerCards)
     {
         base.ApplyEffect(playerCards);
         var invocationCards = BuildValidInvocationCards(playerCards);
         foreach (var invocationCard in invocationCards)
         {
-            invocationCard.Attack += bonusAttack;
-            invocationCard.Defense += bonusDefense;
+            IncrementStats(invocationCard);
         }
     }
-
+    
+    /// <summary>
+    /// Increases the stats of the provided invocation card based on the ability.
+    /// </summary>
+    /// <param name="invocationCard">The invocation card.</param>
+    private void IncrementStats(InGameInvocationCard invocationCard)
+    {
+        invocationCard.Attack += bonusAttack;
+        invocationCard.Defense += bonusDefense;
+    }
+    
+    /// <summary>
+    /// Decreases the stats of the provided invocation card based on the ability.
+    /// </summary>
+    /// <param name="invocationCard">The invocation card.</param>
+    private void DecrementStats(InGameInvocationCard invocationCard)
+    {
+        invocationCard.Attack -= bonusAttack;
+        invocationCard.Defense -= bonusDefense;
+    }
+    
+    /// <summary>
+    /// Handles the logic when a new invocation card is added.
+    /// </summary>
+    /// <param name="invocationCard">The added invocation card.</param>
+    /// <param name="playerCards">The player's cards.</param>
     public override void OnInvocationCardAdded(InGameInvocationCard invocationCard, PlayerCards playerCards)
     {
         base.OnInvocationCardAdded(invocationCard, playerCards);
         if (family != CardFamily.Any && !invocationCard.Families.Contains(family)) return;
-        invocationCard.Attack += bonusAttack;
-        invocationCard.Defense += bonusDefense;
+        IncrementStats(invocationCard);
     }
 
+    /// <summary>
+    /// Handles the logic when the field card is removed from the field.
+    /// </summary>
+    /// <param name="playerCards">The player's cards.</param>
     public override void OnFieldCardRemoved(PlayerCards playerCards)
     {
         base.OnFieldCardRemoved(playerCards);
         var invocationCards = BuildValidInvocationCards(playerCards);
         foreach (var invocationCard in invocationCards)
         {
-            invocationCard.Attack -= bonusAttack;
-            invocationCard.Defense -= bonusDefense;
+            DecrementStats(invocationCard);
         }
     }
 
+    /// <summary>
+    /// Handles the logic when an invocation changes its family.
+    /// </summary>
+    /// <param name="previousFamilies">The previous families of the invocation card.</param>
+    /// <param name="invocationCard">The invocation card that changed families.</param>
     public override void OnInvocationChangeFamily(CardFamily[] previousFamilies, InGameInvocationCard invocationCard)
     {
         base.OnInvocationChangeFamily(previousFamilies, invocationCard);
@@ -65,14 +116,12 @@ public class EarnATKDEFForFamilyAbility : FieldAbility
         {
             if (invocationCard.Families.Contains(family) && !previousFamilies.Contains(family))
             {
-                invocationCard.Attack += bonusAttack;
-                invocationCard.Defense += bonusDefense;
+                IncrementStats(invocationCard);
             }
 
             if (!invocationCard.Families.Contains(family) && previousFamilies.Contains(family))
             {
-                invocationCard.Attack -= bonusAttack;
-                invocationCard.Defense -= bonusDefense;
+                DecrementStats(invocationCard);
             }
         }
     }
