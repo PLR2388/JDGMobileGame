b5c4df334b0ede64e6e7bbdcce1aa148ed06b925
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 05:33:12 +0200

    KillBothCardsIfAttackAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/KillBothCardsIfAttackAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/KillBothCardsIfAttackAbility.cs
@@ -1,15 +1,31 @@
 using _Scripts.Units.Invocation;
 using UnityEngine;
 
+
+/// <summary>
+/// Represents an ability that destroys both the attacker and attacked cards when specific conditions are met.
+/// </summary>
 public class KillBothCardsIfAttackAbility : Ability
 {
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KillBothCardsIfAttackAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
     public KillBothCardsIfAttackAbility(AbilityName name, string description)
     {
         Name = name;
         Description = description;
     }
 
+    /// <summary>
+    /// Handles the scenario when a card attacks another.
+    /// </summary>
+    /// <param name="attackedCard">The card that is attacked.</param>
+    /// <param name="attacker">The card that is performing the attack.</param>
+    /// <param name="playerCards">The collection of the current player's cards.</param>
+    /// <param name="opponentPlayerCards">The collection of the opponent player's cards.</param>
     public override void OnAttackCard(InGameInvocationCard attackedCard, InGameInvocationCard attacker, PlayerCards playerCards,
         PlayerCards opponentPlayerCards)
     {
@@ -23,6 +39,16 @@ public class KillBothCardsIfAttackAbility : Ability
         }
     }
 
+    /// <summary>
+    /// Handles the scenario when a card is attacked by another.
+    /// </summary>
+    /// <param name="canvas">The canvas transform.</param>
+    /// <param name="attackedCard">The card that is attacked.</param>
+    /// <param name="attacker">The card that is performing the attack.</param>
+    /// <param name="playerCards">The collection of the current player's cards.</param>
+    /// <param name="opponentPlayerCards">The collection of the opponent player's cards.</param>
+    /// <param name="currentPlayerStatus">The status of the current player.</param>
+    /// <param name="opponentPlayerStatus">The status of the opponent player.</param>
     public override void OnCardAttacked(Transform canvas, InGameInvocationCard attackedCard,
         InGameInvocationCard attacker, PlayerCards playerCards, PlayerCards opponentPlayerCards, PlayerStatus currentPlayerStatus, PlayerStatus opponentPlayerStatus)
     {
