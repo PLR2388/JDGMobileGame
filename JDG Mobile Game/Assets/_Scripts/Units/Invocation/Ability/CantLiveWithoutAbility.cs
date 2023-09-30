4db07962509cf27a99e9e015e8ec9db9bb98e324
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 14:40:33 +0200

    CantLiveWithoutAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/CantLiveWithoutAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/CantLiveWithoutAbility.cs
@@ -4,11 +4,21 @@ using _Scripts.Units.Invocation;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability that enforces a dependency of the card's existence based on certain card conditions.
+/// </summary>
 public class CantLiveWithoutAbility : Ability
 {
-    private List<string> conditionInvocationCards;
-    private CardFamily family;
+    private readonly List<string> conditionInvocationCards;
+    private readonly CardFamily family;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CantLiveWithoutAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="list">The list of card titles this ability relies on. Defaults to null.</param>
+    /// <param name="family">The family of cards this ability relies on. Defaults to any family.</param>
     public CantLiveWithoutAbility(AbilityName name, string description, List<string> list = null,
         CardFamily family = CardFamily.Any)
     {
@@ -18,7 +28,10 @@ public class CantLiveWithoutAbility : Ability
         this.family = family;
     }
 
-
+    /// <summary>
+    /// Checks and applies the power of the ability, removing the card if conditions aren't met.
+    /// </summary>
+    /// <param name="playerCards">The player's collection of cards.</param>
     private void ApplyPower(PlayerCards playerCards)
     {
         if (CantLive(playerCards))
@@ -28,6 +41,11 @@ public class CantLiveWithoutAbility : Ability
         }
     }
 
+    /// <summary>
+    /// Determines if the card can't live based on its conditions.
+    /// </summary>
+    /// <param name="playerCards">The player's collection of cards.</param>
+    /// <returns>True if the card can't live, otherwise false.</returns>
     private bool CantLive(PlayerCards playerCards)
     {
         if (conditionInvocationCards == null)
@@ -45,17 +63,32 @@ public class CantLiveWithoutAbility : Ability
  
     }
 
+    /// <summary>
+    /// Applies the effect of the ability.
+    /// </summary>
+    /// <param name="canvas">The UI canvas.</param>
+    /// <param name="playerCards">The player's collection of cards.</param>
+    /// <param name="opponentPlayerCards">The opponent's collection of cards.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         ApplyPower(playerCards);
     }
 
+    /// <summary>
+    /// Reactivates the ability effect.
+    /// </summary>
+    /// <param name="playerCards">The player's collection of cards.</param>
     public override void ReactivateEffect(PlayerCards playerCards)
     {
         base.ReactivateEffect(playerCards);
         ApplyPower(playerCards);
     }
 
+    /// <summary>
+    /// Handles the scenario when a card is removed.
+    /// </summary>
+    /// <param name="removeCard">The card that was removed.</param>
+    /// <param name="playerCards">The player's collection of cards.</param>
     public override void OnCardRemove(InGameInvocationCard removeCard, PlayerCards playerCards)
     {
         base.OnCardRemove(removeCard, playerCards);
