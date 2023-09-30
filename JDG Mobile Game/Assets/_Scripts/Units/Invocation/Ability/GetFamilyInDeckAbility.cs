6a6bf892ed352957704832cdd7ec79972a348d90
Paul-Louis <paul-louis.renard@outlook.fr>
2023-09-30 14:19:25 +0200

    GetFamilyInDeckAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/GetFamilyInDeckAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Invocation/Ability/GetFamilyInDeckAbility.cs
@@ -4,10 +4,22 @@ using _Scripts.Units.Invocation;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents an ability to retrieve cards of a specified family from the deck.
+/// </summary>
 public class GetFamilyInDeckAbility : Ability
 {
-    private CardFamily family;
+    /// <summary>
+    /// The family of cards this ability targets.
+    /// </summary>
+    private readonly CardFamily family;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetFamilyInDeckAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="cardFamily">The family of cards this ability is concerned with.</param>
     public GetFamilyInDeckAbility(AbilityName name, string description, CardFamily cardFamily)
     {
         Name = name;
@@ -15,7 +27,13 @@ public class GetFamilyInDeckAbility : Ability
         family = cardFamily;
     }
 
-
+    /// <summary>
+    /// Applies the effect of the ability, prompting the player to select a card from the specified family.
+    /// If the player selects a valid card, it's moved from the deck to the hand.
+    /// </summary>
+    /// <param name="canvas">The game canvas where UI components will be displayed.</param>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="opponentPlayerCards">The opponent's current cards.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCards)
     {
         var config = new MessageBoxConfig(
@@ -37,19 +55,15 @@ public class GetFamilyInDeckAbility : Ability
                         family.ToName()
                     ),
                     familyCards,
-                    showPositiveButton: true,
-                    showNegativeButton: true,
-                    positiveAction: (card) =>
+                    showOkButton: true,
+                    okAction: card =>
                     {
                         if (card == null)
                         {
                             var config = new MessageBoxConfig(
                                 LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.WARNING_TITLE),
                                 LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.WARNING_MUST_CHOOSE_CARD),
-                                showOkButton: true,
-                                okAction: () =>
-                                {
-                                }
+                                showOkButton: true
                             );
                             MessageBox.Instance.CreateMessageBox(
                                 canvas,
@@ -61,9 +75,6 @@ public class GetFamilyInDeckAbility : Ability
                             playerCards.Deck.Remove(card);
                             playerCards.HandCards.Add(card);
                         }
-                    },
-                    negativeAction: () =>
-                    {
                     }
                 );
                 CardSelector.Instance.CreateCardSelection(canvas, config);
