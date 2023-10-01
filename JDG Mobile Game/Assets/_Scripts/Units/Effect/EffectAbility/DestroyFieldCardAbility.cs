cea62c6846b00c8389dd67f94be7b6180fcead1e
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-01 14:26:29 +0200

    DestroyFieldCardAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/DestroyFieldCardAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/DestroyFieldCardAbility.cs
@@ -2,10 +2,19 @@ using System.Collections.Generic;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents the ability to destroy field cards.
+/// </summary>
 public class DestroyFieldCardAbility : EffectAbility
 {
-    private float costHealth;
+    private readonly float costHealth;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DestroyFieldCardAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="cost">The health cost for using the ability.</param>
     public DestroyFieldCardAbility(EffectAbilityName name, string description, float cost)
     {
         Name = name;
@@ -13,12 +22,100 @@ public class DestroyFieldCardAbility : EffectAbility
         costHealth = cost;
     }
 
+    /// <summary>
+    /// Applies the effect of the ability.
+    /// </summary>
+    /// <param name="canvas">The canvas to draw on.</param>
+    /// <param name="playerCards">The cards of the player.</param>
+    /// <param name="opponentPlayerCard">The cards of the opponent player.</param>
+    /// <param name="playerStatus">The status of the player.</param>
+    /// <param name="opponentStatus">The status of the opponent player.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCard,
         PlayerStatus playerStatus,
         PlayerStatus opponentStatus)
     {
         base.ApplyEffect(canvas, playerCards, opponentPlayerCard, playerStatus, opponentStatus);
 
+        var cards = CollectFieldCards(playerCards, opponentPlayerCard);
+
+        var config = new CardSelectorConfig(
+            LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.CARDS_SELECTOR_TITLE_CHOICE_DESTROY_FIELD_CARD),
+            cards,
+            showOkButton: true,
+            okAction: (card) =>
+            {
+                HandleSelectedCard(canvas, playerCards, opponentPlayerCard, playerStatus, card);
+            }
+        );
+        CardSelector.Instance.CreateCardSelection(canvas, config);
+    }
+    
+    /// <summary>
+    /// Handles the selected card.
+    /// </summary>
+    /// <param name="canvas">The canvas to draw on.</param>
+    /// <param name="playerCards">The cards of the player.</param>
+    /// <param name="opponentPlayerCard">The cards of the opponent player.</param>
+    /// <param name="playerStatus">The status of the player.</param>
+    /// <param name="card">The selected card to handle.</param>
+    private void HandleSelectedCard(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCard, PlayerStatus playerStatus, InGameCard card)
+    {
+
+        if (card == null)
+        {
+            var config = new MessageBoxConfig(
+                LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.WARNING_TITLE),
+                LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.WARNING_MUST_CHOOSE_CARD),
+                showOkButton: true
+            );
+            MessageBox.Instance.CreateMessageBox(
+                canvas,
+                config
+            );
+        }
+        else
+        {
+            if (card.CardOwner == CardOwner.Player1)
+            {
+                DestroyField(playerCards, opponentPlayerCard, card);
+            }
+            else
+            {
+                DestroyField(opponentPlayerCard, playerCards, card);
+            }
+            playerStatus.ChangePv(-costHealth);
+        }
+    }
+    
+    /// <summary>
+    /// Destroys a field based on the selected card's owner.
+    /// </summary>
+    /// <param name="playerCards">The cards of the player.</param>
+    /// <param name="opponentPlayerCard">The cards of the opponent player.</param>
+    /// <param name="card">The card to destroy.</param>
+    private static void DestroyField(PlayerCards playerCards, PlayerCards opponentPlayerCard, InGameCard card)
+    {
+
+        if (playerCards.IsPlayerOne)
+        {
+            playerCards.YellowCards.Add(card);
+            playerCards.FieldCard = null;
+        }
+        else
+        {
+            opponentPlayerCard.YellowCards.Add(card);
+            opponentPlayerCard.FieldCard = null;
+        }
+    }
+    
+    /// <summary>
+    /// Collects the field cards from both players.
+    /// </summary>
+    /// <param name="playerCards">The cards of the player.</param>
+    /// <param name="opponentPlayerCard">The cards of the opponent player.</param>
+    /// <returns>A list of field cards from both players.</returns>
+    private static List<InGameCard> CollectFieldCards(PlayerCards playerCards, PlayerCards opponentPlayerCard)
+    {
         var cards = new List<InGameCard>();
         if (playerCards.FieldCard != null)
         {
@@ -29,60 +126,16 @@ public class DestroyFieldCardAbility : EffectAbility
         {
             cards.Add(opponentPlayerCard.FieldCard);
         }
-
-        var config = new CardSelectorConfig(
-            LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.CARDS_SELECTOR_TITLE_CHOICE_DESTROY_FIELD_CARD),
-            cards,
-            showOkButton: true,
-            okAction: (card) =>
-            {
-                if (card == null)
-                {
-                    var config = new MessageBoxConfig(
-                        LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.WARNING_TITLE),
-                        LocalizationSystem.Instance.GetLocalizedValue(LocalizationKeys.WARNING_MUST_CHOOSE_CARD),
-                        showOkButton: true
-                    );
-                    MessageBox.Instance.CreateMessageBox(
-                        canvas,
-                        config
-                    );
-                }
-                else
-                {
-                    if (card.CardOwner == CardOwner.Player1)
-                    {
-                        if (playerCards.IsPlayerOne)
-                        {
-                            playerCards.YellowCards.Add(card);
-                            playerCards.FieldCard = null;
-                        }
-                        else
-                        {
-                            opponentPlayerCard.YellowCards.Add(card);
-                            opponentPlayerCard.FieldCard = null;
-                        }
-                    }
-                    else
-                    {
-                        if (playerCards.IsPlayerOne)
-                        {
-                            opponentPlayerCard.YellowCards.Add(card);
-                            opponentPlayerCard.FieldCard = null;
-                        }
-                        else
-                        {
-                            playerCards.YellowCards.Add(card);
-                            playerCards.FieldCard = null;
-                        }
-                    }
-                    playerStatus.ChangePv(-costHealth);
-                }
-            }
-        );
-        CardSelector.Instance.CreateCardSelection(canvas, config);
+        return cards;
     }
 
+    /// <summary>
+    /// Determines if the effect can be used.
+    /// </summary>
+    /// <param name="playerCards">The cards of the player.</param>
+    /// <param name="opponentPlayerCards">The cards of the opponent player.</param>
+    /// <param name="opponentPlayerStatus">The status of the opponent player.</param>
+    /// <returns>True if the effect can be used, false otherwise.</returns>
     public override bool CanUseEffect(PlayerCards playerCards, PlayerCards opponentPlayerCards,
         PlayerStatus opponentPlayerStatus)
     {
