6313b742b243eeafaef2505476e644b5b97afcdc
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-01 14:18:43 +0200

    DirectAttackEffectAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/DirectAttackEffectAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/DirectAttackEffectAbility.cs
@@ -1,9 +1,17 @@
-using UnityEngine;
 
+/// <summary>
+/// Represents an ability to perform a direct attack based on the opponent's health condition.
+/// </summary>
 public class DirectAttackEffectAbility : EffectAbility
 {
-    private float limitHpOpponent;
+    private readonly float limitHpOpponent;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DirectAttackEffectAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the effect ability.</param>
+    /// <param name="description">The description of the effect ability.</param>
+    /// <param name="limitHpOpponent">The health limit of the opponent to check if the effect can be used.</param>
     public DirectAttackEffectAbility(EffectAbilityName name, string description, float limitHpOpponent)
     {
         Name = name;
@@ -11,6 +19,13 @@ public class DirectAttackEffectAbility : EffectAbility
         this.limitHpOpponent = limitHpOpponent;
     }
 
+    /// <summary>
+    /// Determines if the effect can be used based on the player and opponent's cards and status.
+    /// </summary>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="opponentPlayerCards">The opponent's current cards.</param>
+    /// <param name="opponentPlayerStatus">The opponent's current status.</param>
+    /// <returns><c>true</c> if the opponent's current health is below the set limit and the effect can be used; otherwise, <c>false</c>.</returns>
     public override bool CanUseEffect(PlayerCards playerCards,PlayerCards opponentPlayerCards, PlayerStatus opponentPlayerStatus)
     {
         return opponentPlayerStatus.GetCurrentHealth() < limitHpOpponent;
