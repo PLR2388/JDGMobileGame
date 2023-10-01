de42cc5a0ae59cc82d4d7a5fec83269779dbb188
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-01 05:51:35 +0200

    SwitchEquipmentCardAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Equipment/EquipmentAbility/SwitchEquipmentCardAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Equipment/EquipmentAbility/SwitchEquipmentCardAbility.cs
@@ -1,7 +1,15 @@
 using _Scripts.Units.Invocation;
 
+/// <summary>
+/// Represents the ability of an equipment card to allow a player to switch it with another equipment card.
+/// </summary>
 public class SwitchEquipmentCardAbility : EquipmentAbility
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SwitchEquipmentCardAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the equipment ability.</param>
+    /// <param name="description">The description of the equipment ability.</param>
     public SwitchEquipmentCardAbility(EquipmentAbilityName name, string description)
     {
         Name = name;
@@ -9,6 +17,14 @@ public class SwitchEquipmentCardAbility : EquipmentAbility
         CanAlwaysBePut = true;
     }
 
+    /// <summary>
+    /// Applies the switch equipment card ability effect on the specified invocation card.
+    /// If the invocation card already has an equipment card, the previous one is moved to the yellow cards 
+    /// and its effects are removed from the invocation card.
+    /// </summary>
+    /// <param name="invocationCard">The invocation card affected.</param>
+    /// <param name="playerCards">The cards of the player.</param>
+    /// <param name="opponentPlayerCards">The cards of the opponent player.</param>
     public override void ApplyEffect(InGameInvocationCard invocationCard, PlayerCards playerCards,
         PlayerCards opponentPlayerCards)
     {
