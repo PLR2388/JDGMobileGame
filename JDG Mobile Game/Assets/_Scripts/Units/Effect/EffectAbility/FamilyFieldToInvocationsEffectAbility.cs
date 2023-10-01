40b9721051b3e05146f00dc363fb59b52810b010
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-01 14:12:42 +0200

    FamilyFieldToInvocationsEffectAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/FamilyFieldToInvocationsEffectAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/FamilyFieldToInvocationsEffectAbility.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using _Scripts.Units.Invocation;
+using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Ability effect that applies the family of a field card to invocation cards and handles related logic.
+/// </summary>
 public class FamilyFieldToInvocationsEffectAbility : EffectAbility
 {
-    private string cardName;
-    private float costPerTurn;
+    private readonly string cardName;
+    private readonly float costPerTurn;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FamilyFieldToInvocationsEffectAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the ability.</param>
+    /// <param name="description">The description of the ability.</param>
+    /// <param name="costPerTurn">Cost incurred per turn.</param>
+    /// <param name="cardName">The name of the card.</param>
     public FamilyFieldToInvocationsEffectAbility(EffectAbilityName name, string description, float costPerTurn, string cardName)
     {
         Name = name;
@@ -18,23 +26,52 @@ public class FamilyFieldToInvocationsEffectAbility : EffectAbility
         this.cardName = cardName;
     }
 
+    /// <summary>
+    /// Applies the power of the effect on the player's cards.
+    /// </summary>
+    /// <param name="playerCards">The player's cards.</param>
     private void ApplyPower(PlayerCards playerCards)
     {
         var fieldFamily = playerCards.FieldCard.Family;
         foreach (var invocationCard in playerCards.InvocationCards)
         {
-            invocationCard.Families = new[]
-            {
-                fieldFamily
-            };
+            SetNewFamilyToInvocation(invocationCard, fieldFamily);
         }
     }
+    
+    /// <summary>
+    /// Sets a new family to the specified invocation card.
+    /// </summary>
+    /// <param name="invocationCard">The invocation card.</param>
+    /// <param name="fieldFamily">The new card family.</param>
+    private static void SetNewFamilyToInvocation(InGameInvocationCard invocationCard, CardFamily fieldFamily)
+    {
+        invocationCard.Families = new[]
+        {
+            fieldFamily
+        };
+    }
 
+    /// <summary>
+    /// Determines whether the effect can be used based on the player's cards and opponent's status.
+    /// </summary>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="opponentPlayerCards">The opponent player's current cards.</param>
+    /// <param name="opponentPlayerStatus">The opponent player's current status.</param>
+    /// <returns><c>true</c> if the effect can be used; otherwise, <c>false</c>.</returns>
     public override bool CanUseEffect(PlayerCards playerCards, PlayerCards opponentPlayerCards, PlayerStatus opponentPlayerStatus)
     {
         return playerCards.FieldCard != null && playerCards.InvocationCards.Count > 0;
     }
 
+    /// <summary>
+    /// Applies the effect on the player's cards.
+    /// </summary>
+    /// <param name="canvas">Canvas used for UI operations.</param>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="opponentPlayerCard">The opponent player's cards.</param>
+    /// <param name="playerStatus">The player's current status.</param>
+    /// <param name="opponentStatus">The opponent's current status.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCard,
         PlayerStatus playerStatus,
         PlayerStatus opponentStatus)
@@ -42,6 +79,14 @@ public class FamilyFieldToInvocationsEffectAbility : EffectAbility
         ApplyPower(playerCards);
     }
 
+    /// <summary>
+    /// Handles logic at the start of the turn.
+    /// </summary>
+    /// <param name="canvas">Canvas used for UI operations.</param>
+    /// <param name="playerStatus">The player's current status.</param>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="opponentPlayerStatus">The opponent player's current status.</param>
+    /// <param name="opponentPlayerCards">The opponent player's current cards.</param>
     public override void OnTurnStart(Transform canvas, PlayerStatus playerStatus, PlayerCards playerCards, PlayerStatus opponentPlayerStatus, PlayerCards opponentPlayerCards)
     {
         var config = new MessageBoxConfig(
@@ -58,28 +103,44 @@ public class FamilyFieldToInvocationsEffectAbility : EffectAbility
             },
             negativeAction: () =>
             {
-                foreach (var invocationCard in playerCards.InvocationCards)
-                {
-                    invocationCard.Families = invocationCard.BaseInvocationCard.BaseInvocationCardStats.Families;
-                }
-
-                var effectCard = playerCards.EffectCards.First(effectCard => effectCard.Title == cardName);
-                playerCards.EffectCards.Remove(effectCard);
-                playerCards.YellowCards.Add(effectCard);
+                ResetInvocationsFamily(playerCards);
             }
         );
         MessageBox.Instance.CreateMessageBox(canvas, config);
     }
+    
+    /// <summary>
+    /// Resets the family of invocation cards and moves the effect card back to the yellow pile.
+    /// </summary>
+    /// <param name="playerCards">The player's current cards.</param>
+    private void ResetInvocationsFamily(PlayerCards playerCards)
+    {
+        foreach (var invocationCard in playerCards.InvocationCards)
+        {
+            invocationCard.Families = invocationCard.BaseInvocationCard.BaseInvocationCardStats.Families;
+        }
 
+        var effectCard = playerCards.EffectCards.First(effectCard => effectCard.Title == cardName);
+        playerCards.EffectCards.Remove(effectCard);
+        playerCards.YellowCards.Add(effectCard);
+    }
+
+    /// <summary>
+    /// Handles logic when an invocation card is added.
+    /// </summary>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="invocationCard">The added invocation card.</param>
     public override void OnInvocationCardAdded(PlayerCards playerCards, InGameInvocationCard invocationCard)
     {
         base.OnInvocationCardAdded(playerCards, invocationCard);
-        invocationCard.Families = new[]
-        {
-            playerCards.FieldCard.Family
-        };
+        SetNewFamilyToInvocation(invocationCard, playerCards.FieldCard.Family);
     }
 
+    /// <summary>
+    /// Handles logic when an invocation card is removed.
+    /// </summary>
+    /// <param name="playerCards">The player's current cards.</param>
+    /// <param name="invocationCard">The removed invocation card.</param>
     public override void OnInvocationCardRemoved(PlayerCards playerCards, InGameInvocationCard invocationCard)
     {
         base.OnInvocationCardRemoved(playerCards, invocationCard);
