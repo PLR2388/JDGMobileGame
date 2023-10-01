ad27b1f018ade2b3414938e17f5ca32e0f74fef8
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-01 08:24:51 +0200

    InGameEffectCard

--- JDG Mobile Game/Assets/_Scripts/Units/Effect/InGameEffectCard.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Effect/InGameEffectCard.cs
@@ -3,12 +3,23 @@ using System.Linq;
 
 namespace Cards.EffectCards
 {
+    /// <summary>
+    /// Represents an in-game effect card, which is derived from a base effect card and has additional in-game properties and behaviors.
+    /// </summary>
     public class InGameEffectCard : InGameCard
     {
-        private EffectCard baseEffectCard;
+        private readonly EffectCard baseEffectCard;
 
+        /// <summary>
+        /// List of effect abilities associated with this card.
+        /// </summary>
         public List<EffectAbility> EffectAbilities = new List<EffectAbility>();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InGameEffectCard"/> class.
+        /// </summary>
+        /// <param name="effectCard">The base effect card from which the in-game card is derived.</param>
+        /// <param name="cardOwner">The owner of the card.</param>
         public InGameEffectCard(EffectCard effectCard, CardOwner cardOwner)
         {
             baseEffectCard = effectCard;
@@ -16,6 +27,9 @@ namespace Cards.EffectCards
             Reset();
         }
 
+        /// <summary>
+        /// Resets the in-game card properties to match those of the base effect card.
+        /// </summary>
         private void Reset()
         {
             title = baseEffectCard.Title;
