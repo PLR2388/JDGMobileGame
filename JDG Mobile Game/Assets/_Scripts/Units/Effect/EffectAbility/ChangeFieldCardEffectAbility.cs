827c47680474391cfaff360229693db362f395c0
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-03 05:40:59 +0200

    ChangeFieldCardEffectAbility

--- JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/ChangeFieldCardEffectAbility.cs
+++ JDG Mobile Game/Assets/_Scripts/Units/Effect/EffectAbility/ChangeFieldCardEffectAbility.cs
@@ -1,11 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents an effect ability that allows changing the Field card.
+/// </summary>
 public class ChangeFieldCardEffectAbility : EffectAbility
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChangeFieldCardEffectAbility"/> class.
+    /// </summary>
+    /// <param name="name">The name of the effect ability.</param>
+    /// <param name="description">The description of the effect ability.</param>
+    /// <param name="numberTurn">The number of turns for which the effect lasts.</param>
     public ChangeFieldCardEffectAbility(EffectAbilityName name, string description, int numberTurn)
     {
         Name = name;
@@ -13,11 +20,26 @@ public class ChangeFieldCardEffectAbility : EffectAbility
         NumberOfTurn = numberTurn;
     }
 
+    /// <summary>
+    /// Determines if the effect can be used based on the player's card deck.
+    /// </summary>
+    /// <param name="playerCards">The cards held by the player.</param>
+    /// <param name="opponentPlayerCards">The cards held by the opponent.</param>
+    /// <param name="opponentPlayerStatus">The status of the opponent player.</param>
+    /// <returns>True if the effect can be used; otherwise, false.</returns>
     public override bool CanUseEffect(PlayerCards playerCards, PlayerCards opponentPlayerCards, PlayerStatus opponentPlayerStatus)
     {
         return playerCards.Deck.Any(card => card.Type == CardType.Field);
     }
 
+    /// <summary>
+    /// Applies the effect of changing the Field card.
+    /// </summary>
+    /// <param name="canvas">Transform reference for positioning UI elements.</param>
+    /// <param name="playerCards">The cards held by the player.</param>
+    /// <param name="opponentPlayerCard">The cards held by the opponent.</param>
+    /// <param name="playerStatus">The current status of the player.</param>
+    /// <param name="opponentStatus">The current status of the opponent.</param>
     public override void ApplyEffect(Transform canvas, PlayerCards playerCards, PlayerCards opponentPlayerCard,
         PlayerStatus playerStatus,
         PlayerStatus opponentStatus)
