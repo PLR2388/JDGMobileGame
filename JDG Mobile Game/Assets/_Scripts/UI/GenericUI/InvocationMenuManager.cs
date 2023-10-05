4a9123f8a38d61a0348a0eeb64e2620a9d802291
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-05 05:58:05 +0200

    InvocationMenuManager

--- JDG Mobile Game/Assets/_Scripts/UI/GenericUI/InvocationMenuManager.cs
+++ JDG Mobile Game/Assets/_Scripts/UI/GenericUI/InvocationMenuManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Manages the invocation menu, including its buttons and their states.
+/// </summary>
 public class InvocationMenuManager : StaticInstance<InvocationMenuManager>
 {
     [SerializeField] private GameObject invocationMenu;
      private Button attackButton;
      private Button actionButton;
 
+     /// <summary>
+     /// Initialize the manager, caching necessary components.
+     /// </summary>
     protected override void Awake()
     {
         base.Awake();
@@ -17,11 +23,18 @@ public class InvocationMenuManager : StaticInstance<InvocationMenuManager>
             actionButton = invocationMenu.transform.GetChild(1).GetComponent<Button>();
     }
 
+     /// <summary>
+     /// Updates the state of the attack button based on whether the attacker can attack.
+     /// </summary>
     public void UpdateAttackButton()
     {
         attackButton.interactable = CardManager.Instance.CanAttackerAttack();
     }
 
+     /// <summary>
+     /// Displays the invocation menu and sets button states based on game conditions.
+     /// </summary>
+     /// <param name="isAttackPhase">If set to <c>true</c>, indicates that the game is in the attack phase.</param>
     public void Display(bool isAttackPhase)
     {
         var mousePosition = InputManager.TouchPosition;
@@ -38,11 +51,17 @@ public class InvocationMenuManager : StaticInstance<InvocationMenuManager>
         invocationMenu.transform.position = mousePosition;
     }
 
+     /// <summary>
+     /// Hides the invocation menu.
+     /// </summary>
     public void Hide()
     {
         invocationMenu.SetActive(false);
     }
-
+     
+     /// <summary>
+     /// Enables the attack button.
+     /// </summary>
     public void Enable()
     {
         attackButton.interactable = true;
