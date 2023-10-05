d804974f257dda0725000ffffb7ebaf4d9a57f96
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-05 17:17:45 +0200

    SoundList

--- JDG Mobile Game/Assets/_Scripts/Sound/SoundList.cs
+++ JDG Mobile Game/Assets/_Scripts/Sound/SoundList.cs
@@ -1,12 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+/// <summary>
+/// Enumerates the various musical themes available in the game.
+/// </summary>
 public enum Music
 {
     MainTheme, CanardCity, DrawPhase, Fight, OnePlayerMenu, OptionMenu, Rpg, TwoPlayerMenu, Wizard
 }
 
+/// <summary>
+/// Enumerates various sound effects available in the game.
+/// </summary>
 public enum SoundEffect
 {
     Back,
