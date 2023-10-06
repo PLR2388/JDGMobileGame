7a9939b06fbf8d587f0ba5bc0d1f7463e9bc3a0f
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-06 05:47:02 +0200

    VideoPlayerObserver

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/VideoPlayerObserver.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/VideoPlayerObserver.cs
@@ -2,18 +2,46 @@ using OnePlayer.DialogueBox;
 using UnityEngine;
 using UnityEngine.Video;
 
+/// <summary>
+/// Observes a VideoPlayer component and provides functionality when the video reaches its end point.
+/// </summary>
 public class VideoPlayerObserver : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
 
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Initializes the VideoPlayerObserver by acquiring the VideoPlayer component and setting up event listeners.
+    /// </summary>
+    private void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
-        videoPlayer.loopPointReached += CheckOver;
+
+        if(videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+        else
+        {
+            Debug.LogError("No VideoPlayer component found on this GameObject!", this);
+        }
+    }
+    
+    /// <summary>
+    /// Ensures that event listeners are cleaned up when the object is destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+        if(videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= OnVideoEnd;
+        }
     }
 
-    void CheckOver(VideoPlayer vp)
+    /// <summary>
+    /// Called when the video reaches its end. Triggers the corresponding dialogue event and deactivates the game object.
+    /// </summary>
+    /// <param name="vp">The VideoPlayer that has reached its end point.</param>
+    private void OnVideoEnd(VideoPlayer vp)
     {
         DialogueUI.TriggerDoneEvent.Invoke(NextDialogueTrigger.EndVideo);
         gameObject.SetActive(false);
