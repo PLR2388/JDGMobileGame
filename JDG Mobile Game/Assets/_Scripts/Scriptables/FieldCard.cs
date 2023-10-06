6ed9e16434d436b34fdd41f3794dc0d1c8112dce
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-06 05:28:17 +0200

    FieldCard

--- JDG Mobile Game/Assets/_Scripts/Scriptables/FieldCard.cs
+++ JDG Mobile Game/Assets/_Scripts/Scriptables/FieldCard.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 
 namespace Cards.FieldCards
 {
+    /// <summary>
+    /// Represents a field card in the game, extending the base functionality of the Card class.
+    /// </summary>
     [CreateAssetMenu(fileName = "New Card", menuName = "FieldCard")]
     public class FieldCard : Card
     {
         [SerializeField] private CardFamily family;
+        
+        /// <summary>
+        /// Gets the family to which this field card belongs.
+        /// </summary>
         public CardFamily Family => family;
 
+        /// <summary>
+        /// List of abilities that are associated with this field card.
+        /// </summary>
         public List<FieldAbilityName> FieldAbilities = new List<FieldAbilityName>();
 
         private void Awake()
