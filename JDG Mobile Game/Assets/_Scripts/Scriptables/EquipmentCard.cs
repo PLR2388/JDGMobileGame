3ea53478354d05407cfe159fbfecb557f5ac78f3
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-06 05:30:39 +0200

    EquipmentCard

--- JDG Mobile Game/Assets/_Scripts/Scriptables/EquipmentCard.cs
+++ JDG Mobile Game/Assets/_Scripts/Scriptables/EquipmentCard.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 namespace Cards.EquipmentCards
 {
+    /// <summary>
+    /// Represents an equipment card in the game, extending the base functionality of the Card class.
+    /// </summary>
     [CreateAssetMenu(fileName = "New Card", menuName = "EquipmentCard")]
     public class EquipmentCard : Card
     {
+        /// <summary>
+        /// List of abilities that are associated with this equipment card.
+        /// </summary>
         public List<EquipmentAbilityName> EquipmentAbilities = new List<EquipmentAbilityName>();
 
         private void Awake()
