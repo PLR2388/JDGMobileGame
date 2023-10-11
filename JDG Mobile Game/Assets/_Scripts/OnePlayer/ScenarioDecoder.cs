d0854c30c0396da31b9e4ffbf7bb2b154efc3f2b
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-11 05:32:25 +0200

    ScenarioDecoder

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/ScenarioDecoder.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/ScenarioDecoder.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
+/// <summary>
+/// Decodes a JSON-based scenario from a text asset into a Scenario object.
+/// </summary>
 public class ScenarioDecoder : MonoBehaviour
 {
+    /// <summary>
+    /// The JSON text asset containing scenario data.
+    /// </summary>
     public TextAsset jsonFile;
-    public DialogueObject dialogueObject;
 
+    /// <summary>
+    /// Retrieves a Scenario object parsed from the associated JSON text asset.
+    /// </summary>
     public Scenario Scenario
     {
         get
         {
+            // Convert the JSON text into a JsonScenario object
             JsonScenario jsonScenario = JsonUtility.FromJson<JsonScenario>(jsonFile.text);
 
+            // Convert the JsonScenario object into a Scenario object and return
             Scenario scenario = jsonScenario.ToScenario();
             return scenario;
         }
