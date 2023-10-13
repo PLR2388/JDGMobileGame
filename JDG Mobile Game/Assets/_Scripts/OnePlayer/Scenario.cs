b46f220899e5080e06079587c713baa22c114500
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-13 05:26:39 +0200

    Scenario

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/Scenario.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/Scenario.cs
@@ -1,80 +1,187 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 
+/// <summary>
+/// Represents the structured data of a game scenario.
+/// </summary>
 public class Scenario
 {
-    public ActionScenario[] ActionScenarios;
+    /// <summary>
+    /// Collection of actions defined in the scenario.
+    /// </summary>
+    public ActionScenario[] ActionScenarios { get; set; }
 }
 
+/// <summary>
+/// Represents a single action that can be taken during a scenario.
+/// </summary>
 public class ActionScenario
 {
+    /// <summary>
+    /// Index of the action in the scenario.
+    /// </summary>
     public int Index;
+    
+    /// <summary>
+    /// Type of highlight associated with the action.
+    /// </summary>
     public Highlight Highlight;
+    
+    /// <summary>
+    /// Card associated with the action.
+    /// </summary>
     public string PutCard;
+    
+    /// <summary>
+    /// Image associated with the action.
+    /// </summary>
     public string Image;
+    
+    /// <summary>
+    /// Video associated with the action.
+    /// </summary>
     public string Video;
+    
+    /// <summary>
+    /// Attack sequences associated with the action.
+    /// </summary>
     public string[] Attack;
+    
+    /// <summary>
+    /// Type of action to be taken.
+    /// </summary>
     public Action Action;
 }
 
-[System.Serializable]
+/// <summary>
+/// Represents the raw JSON data for a scenario.
+/// </summary>
+[Serializable]
 public class JsonScenario
 {
     public ActionJsonScenario[] jsonScenarios;
 
+    /// <summary>
+    /// Converts the JSON data into a structured Scenario object.
+    /// </summary>
+    /// <returns>A Scenario object derived from the JSON data.</returns>
     public Scenario ToScenario()
     {
         var scenario = new Scenario();
-        var actionScenarios = new ArrayList();
+        var actionScenarios = new List<ActionScenario>();
         foreach (var actionJson in jsonScenarios)
         {
-            var actionScenario = new ActionScenario
-            {
-                Index = actionJson.index
-            };
-            if (actionJson.highlight == null)
-            {
-                actionScenario.Highlight = Highlight.unknown;
-            }
-            else
-            {
-                Enum.TryParse(actionJson.highlight, out actionScenario.Highlight);
-            }
-
-            if (actionJson.action == null)
-            {
-                actionScenario.Action = Action.unknown;
-            }
-            else
-            {
-                Enum.TryParse(actionJson.action, out actionScenario.Action);
-            }
-        
-            actionScenario.PutCard = actionJson.putCard;
-            actionScenario.Image = actionJson.image;
-            actionScenario.Video = actionJson.video;
-            actionScenario.Attack = actionJson.attack?.Split('>');
+            var actionScenario = ToActionScenario(actionJson);
             actionScenarios.Add(actionScenario);
         }
 
-        scenario.ActionScenarios = (ActionScenario[])actionScenarios.ToArray(typeof(ActionScenario));
+        scenario.ActionScenarios = actionScenarios.ToArray();
         return scenario;
+    }
+    
+    /// <summary>
+    /// Converts a JSON action representation to an ActionScenario object.
+    /// </summary>
+    /// <param name="actionJson">The JSON representation of the action.</param>
+    /// <returns>An ActionScenario derived from the JSON data.</returns>
+    private static ActionScenario ToActionScenario(ActionJsonScenario actionJson)
+    {
+        var actionScenario = new ActionScenario
+        {
+            Index = actionJson.index
+        };
+        
+        SetHighlightFromActionJson(actionJson, actionScenario);
+        SetActionFromActionJson(actionJson, actionScenario);
+
+        actionScenario.PutCard = actionJson.putCard;
+        actionScenario.Image = actionJson.image;
+        actionScenario.Video = actionJson.video;
+        actionScenario.Attack = actionJson.attack?.Split('>');
+        return actionScenario;
+    }
+    
+    /// <summary>
+    /// Sets the action type for an ActionScenario based on the JSON representation.
+    /// </summary>
+    /// <param name="actionJson">The JSON representation of the action.</param>
+    /// <param name="actionScenario">The ActionScenario object to be modified.</param>
+    private static void SetActionFromActionJson(ActionJsonScenario actionJson, ActionScenario actionScenario)
+    {
+        if (actionJson.action == null)
+        {
+            actionScenario.Action = Action.unknown;
+        }
+        else
+        {
+            Enum.TryParse(actionJson.action, out actionScenario.Action);
+        }
+    }
+    
+    /// <summary>
+    /// Sets the highlight type for an ActionScenario based on the JSON representation.
+    /// </summary>
+    /// <param name="actionJson">The JSON representation of the action.</param>
+    /// <param name="actionScenario">The ActionScenario object to be modified.</param>
+    private static void SetHighlightFromActionJson(ActionJsonScenario actionJson, ActionScenario actionScenario)
+    {
+
+        if (actionJson.highlight == null)
+        {
+            actionScenario.Highlight = Highlight.unknown;
+        }
+        else
+        {
+            Enum.TryParse(actionJson.highlight, out actionScenario.Highlight);
+        }
     }
 }
 
-[System.Serializable]
+/// <summary>
+/// Represents the raw JSON data for an action within a scenario.
+/// </summary>
+[Serializable]
 public class ActionJsonScenario
 {
+    /// <summary>
+    /// Index of the action in the JSON data.
+    /// </summary>
     public int index;
+    
+    /// <summary>
+    /// Type of highlight in the JSON data.
+    /// </summary>
     public string highlight;
+    
+    /// <summary>
+    /// Card information in the JSON data.
+    /// </summary>
     public string putCard;
-    public string trigger;
+    
+    /// <summary>
+    /// Image information in the JSON data.
+    /// </summary>
     public string image;
+    
+    /// <summary>
+    /// Video information in the JSON data.
+    /// </summary>
     public string video;
+    
+    /// <summary>
+    /// Attack sequences in the JSON data.
+    /// </summary>
     public string attack;
+    
+    /// <summary>
+    /// Type of action in the JSON data.
+    /// </summary>
     public string action;
 }
 
+/// <summary>
+/// Enumerates possible highlights in a scenario.
+/// </summary>
 public enum Highlight
 {
     space,
@@ -90,6 +197,9 @@ public enum Highlight
     life_point
 }
 
+/// <summary>
+/// Enumerates possible actions that can be taken in a scenario.
+/// </summary>
 public enum Action
 {
     next_phase,
