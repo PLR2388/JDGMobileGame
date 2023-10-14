3bc30c7795abb105ed196556503477cf10aab702
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-14 05:36:17 +0200

    HightLightText

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/HightLightText.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/HightLightText.cs
@@ -3,20 +3,62 @@ using OnePlayer;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Handles the highlighting of text based on game events.
+/// </summary>
 public class HightLightText : MonoBehaviour
 {
+    /// <summary>
+    /// The highlight element associated with this text.
+    /// </summary>
     [SerializeField] private HighlightElement element;
         
-    private bool isActivated = false;
+    /// <summary>
+    /// Flag to indicate if the highlight is activated.
+    /// </summary>
+    private bool isActivated;
+    
+    /// <summary>
+    /// Flag to manage the pulse coroutine execution.
+    /// </summary>
     private bool waitEndTurn = true;
+    
+    /// <summary>
+    /// Cached reference to the TextMeshProUGUI component.
+    /// </summary>
+    private TextMeshProUGUI textMesh;
+    
+    /// <summary>
+    /// Cache references on awake.
+    /// </summary>
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
         
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Subscribe to relevant game events on start.
+    /// </summary>
+    private void Start()
     {
         HighLightPlane.Highlight.AddListener(UpdateStatus);
     }
+    
+    /// <summary>
+    /// Unsubscribe from events when the object is destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+        // Unsubscribe from the event when this object is destroyed
+        HighLightPlane.Highlight.RemoveListener(UpdateStatus);
+    }
 
-    void UpdateStatus(HighlightElement highlightElement, bool activated)
+    /// <summary>
+    /// Updates the status based on game events.
+    /// </summary>
+    /// <param name="highlightElement">The received highlight element.</param>
+    /// <param name="activated">Flag indicating if the highlight is activated.</param>
+    private void UpdateStatus(HighlightElement highlightElement, bool activated)
     {
         if (highlightElement == element)
         {
@@ -24,30 +66,45 @@ public class HightLightText : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Handles the text update on each frame.
+    /// </summary>
+    private void Update()
     {
         if (isActivated)
         {
             if (waitEndTurn)
             {
-                StartCoroutine("Pulse");
+                StartCoroutine(PulseCoroutine());
             }
         }
         else
         {
-            gameObject.GetComponent<TextMeshProUGUI>().color = Color.white;
+            SetTextColor(Color.white);
             waitEndTurn = true;
         }
     }
+    
+    /// <summary>
+    /// Sets the text color.
+    /// </summary>
+    /// <param name="color">The desired color.</param>
+    private void SetTextColor(Color color)
+    {
+        textMesh.color = color;
+    }
 
-    IEnumerator Pulse()
+    /// <summary>
+    /// Coroutine for the pulse effect on the text.
+    /// </summary>
+    /// <returns>IEnumerator for the coroutine.</returns>
+    private IEnumerator PulseCoroutine()
     {
         waitEndTurn = false;
         yield return new WaitForSeconds(0.5f);
-        gameObject.GetComponent<TextMeshProUGUI>().color = Color.white;
+        SetTextColor(Color.white);
         yield return new WaitForSeconds(0.5f);
-        gameObject.GetComponent<TextMeshProUGUI>().color = Color.clear;
+        SetTextColor(Color.clear);
         waitEndTurn = true;
     }
 }
