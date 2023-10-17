a7e848c6784662104794fe5f90493f3e93071e91
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-17 05:32:57 +0200

    HighLightPhysicalCard

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/HighLightPhysicalCard.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/HighLightPhysicalCard.cs
@@ -3,54 +3,91 @@ using UnityEngine;
 
 namespace OnePlayer
 {
+    /// <summary>
+    /// Provides functionality to highlight a physical card in the game.
+    /// </summary>
     public class HighLightPhysicalCard : MonoBehaviour
     {
         [SerializeField] private HighlightElement element;
+        [SerializeField] private Color pulseColor = Color.green;
+        [SerializeField] private Color defaultColor = Color.white;
+
+
+        private static readonly string TargetCardName = CardNameMappings.CardNameMap[CardNames.Tentacules];
 
         private bool isActivated = false;
         private bool waitEndTurn = true;
 
-        // Start is called before the first frame update
-        void Start()
+        private MeshRenderer meshRenderer;
+        private PhysicalCardDisplay cardDisplay;
+
+        /// <summary>
+        /// Called when the script instance is being loaded.
+        /// </summary>
+        private void Awake()
         {
-            OnePlayer.HighLightPlane.Highlight.AddListener(UpdateStatus);
+            meshRenderer = GetComponent<MeshRenderer>();
+            cardDisplay = GetComponent<PhysicalCardDisplay>();
         }
 
-        void UpdateStatus(HighlightElement highlightElement, bool activated)
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// </summary>
+        private void Start()
         {
-            if (highlightElement == element)
+            HighLightPlane.Highlight.AddListener(UpdateStatus);
+        }
+
+        /// <summary>
+        /// Unsubscribe from events when this object is being destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            HighLightPlane.Highlight.RemoveListener(UpdateStatus);
+        }
+
+        /// <summary>
+        /// Updates the activation status of the highlight.
+        /// </summary>
+        /// <param name="highlightElement">The element to be checked.</param>
+        /// <param name="activated">Whether the element is activated.</param>
+        private void UpdateStatus(HighlightElement highlightElement, bool activated)
+        {
+            if (highlightElement == element && cardDisplay.Card.Title == TargetCardName)
             {
-                if (GetComponent<PhysicalCardDisplay>().Card.Title == "Tentacules")
-                {
-                    isActivated = activated;
-                }
+                isActivated = activated;
             }
         }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Update is called once per frame.
+        /// </summary>
+        private void Update()
         {
             if (isActivated)
             {
                 if (waitEndTurn)
                 {
-                    StartCoroutine("Pulse");
+                    StartCoroutine(PulseCoroutine());
                 }
             }
             else
             {
-                gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+                meshRenderer.material.color = defaultColor;
                 waitEndTurn = true;
             }
         }
-
-        IEnumerator Pulse()
+        
+        /// <summary>
+        /// Coroutine to handle the pulsing effect on the card.
+        /// </summary>
+        private IEnumerator PulseCoroutine()
         {
             waitEndTurn = false;
             yield return new WaitForSeconds(0.5f);
-            gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+            meshRenderer.material.color = pulseColor;
             yield return new WaitForSeconds(0.5f);
-            gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            meshRenderer.material.color = defaultColor;
             waitEndTurn = true;
         }
     }
