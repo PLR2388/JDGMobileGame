246e7f027d2def08f2e39758d4e9a72793f82a31
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-18 18:29:41 +0200

    TypewriterEffect

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/DialogueBox/TypewriterEffect.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/DialogueBox/TypewriterEffect.cs
@@ -4,27 +4,59 @@ using UnityEngine;
 
 namespace OnePlayer.DialogueBox
 {
+    /// <summary>
+    /// Applies a typewriter effect to the given text over time.
+    /// </summary>
     public class TypewriterEffect : MonoBehaviour
     {
-        private float typewriterSpeed = 25f;
+        [Tooltip("Default typing speed")]
+        [SerializeField] private float defaultTypewriterSpeed = 25f;
+        private float currentTypewriterSpeed;
+        
+        /// <summary>
+        /// Initializes the current typing speed to its default value.
+        /// </summary>
+        private void Start()
+        {
+            currentTypewriterSpeed = defaultTypewriterSpeed;
+        }
 
+        /// <summary>
+        /// Adapts the typing speed based on desired duration and text length.
+        /// </summary>
+        /// <param name="duration">The desired duration for the text to be fully typed.</param>
+        /// <param name="lengthText">The length of the text being typed.</param>
         public void AdaptSpeedToLength(float duration, int lengthText)
         {
             if (duration > 0 && lengthText > 0)
             {
-                typewriterSpeed = lengthText / duration;
+                currentTypewriterSpeed = lengthText / duration;
             }
             else
             {
-                typewriterSpeed = 25f;
+                currentTypewriterSpeed = defaultTypewriterSpeed;
             }
         }
 
+        /// <summary>
+        /// Begins the typewriter effect on the provided text and label.
+        /// </summary>
+        /// <param name="textToType">The text to apply the typewriter effect on.</param>
+        /// <param name="textLabel">The TMP_Text component to display the typed text.</param>
+        /// <returns>Coroutine to handle the typing effect over time.</returns>
         public Coroutine Run(string textToType, TMP_Text textLabel)
         {
-           return StartCoroutine(TypeText(textToType, textLabel));
+            if (textLabel != null) return StartCoroutine(TypeText(textToType, textLabel));
+            Debug.LogError("Text label is null!");
+            return null;
         }
 
+        /// <summary>
+        /// Coroutine that types out the text character by character at the specified speed.
+        /// </summary>
+        /// <param name="textToType">The text to type out.</param>
+        /// <param name="textLabel">The TMP_Text component to display the typed text.</param>
+        /// <returns>An enumerator needed for the coroutine to operate.</returns>
         private IEnumerator TypeText(string textToType, TMP_Text textLabel)
         {
             textLabel.text = string.Empty;
@@ -34,7 +66,7 @@ namespace OnePlayer.DialogueBox
 
             while (charIndex < textToType.Length)
             {
-                t += Time.deltaTime * typewriterSpeed;
+                t += Time.deltaTime * currentTypewriterSpeed;
                 charIndex = Mathf.FloorToInt(t);
                 charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
