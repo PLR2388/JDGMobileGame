6dab5c8f681284c45ddcbb83738e0095dd1009ac
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-19 05:29:05 +0200

    Response

--- JDG Mobile Game/Assets/_Scripts/OnePlayer/DialogueBox/Response.cs
+++ JDG Mobile Game/Assets/_Scripts/OnePlayer/DialogueBox/Response.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 
+/// <summary>
+/// Represents a player's potential response in a dialogue system.
+/// </summary>
 [System.Serializable]
 public class Response
 {
     [SerializeField] private string responseText;
     [SerializeField] private DialogueObject dialogueObject;
 
+    /// <summary>
+    /// Gets the text of the response.
+    /// </summary>
     public string ResponseText => responseText;
 
+    /// <summary>
+    /// Gets the dialogue object associated with this response.
+    /// </summary>
     public DialogueObject DialogueObject => dialogueObject;
 }
