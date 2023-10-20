233e1842fac8d69aa2402a9c71d033b822f4f627
Paul-Louis <paul-louis.renard@outlook.fr>
2023-10-20 05:35:34 +0200

    TutoHandCardDisplay

--- JDG Mobile Game/Assets/_Scripts/TutoHandCardDisplay.cs
+++ JDG Mobile Game/Assets/_Scripts/TutoHandCardDisplay.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using Cards;
 using UnityEngine;
 
+/// <summary>
+/// Represents the display of cards in a tutorial scenario.
+/// </summary>
 public class TutoHandCardDisplay : HandCardDisplay
 {
 
     private int currentDialogIndex = 0;
 
-    /// <summary>
-    /// Called when the script instance is being loaded.
-    /// Subscribes to relevant events.
-    /// </summary>
-    private void Awake()
-    {
-        SubscribeToEvents();
-    }
-    
+    private const int StartHighlightMusiqueDeMegadriveIndex = 35;
+
     /// <summary>
     /// Called when the object becomes enabled and active.
     /// Subscribes to relevant events.
@@ -25,7 +20,7 @@ public class TutoHandCardDisplay : HandCardDisplay
     {
         SubscribeToEvents();
     }
-    
+
     /// <summary>
     /// Called when the behaviour becomes disabled.
     /// Unsubscribes from events and clears created cards.
@@ -35,16 +30,23 @@ public class TutoHandCardDisplay : HandCardDisplay
         ClearCreatedCards();
     }
 
+    /// <summary>
+    /// Called before the object is destroyed.
+    /// Cleans up by clearing created cards and unsubscribing from events.
+    /// </summary>
     private void OnDestroy()
     {
         ClearCreatedCards();
         UnsubscribeFromEvents();
     }
 
+    /// <summary>
+    /// Subscribes to necessary events for card display updates.
+    /// </summary>
     private void SubscribeToEvents()
     {
         HandCardChange.AddListener(DisplayHandCard);
-        DialogueUI.DialogIndex.AddListener(SavedIndexDialog);
+        DialogueUI.DialogIndex.AddListener(UpdateCurrentDialogIndex);
     }
 
     /// <summary>
@@ -53,13 +55,26 @@ public class TutoHandCardDisplay : HandCardDisplay
     private void UnsubscribeFromEvents()
     {
         HandCardChange.RemoveListener(DisplayHandCard);
-        DialogueUI.DialogIndex.RemoveListener(SavedIndexDialog);
+        DialogueUI.DialogIndex.RemoveListener(UpdateCurrentDialogIndex);
     }
-    
-    private void SavedIndexDialog(int index)
+
+    /// <summary>
+    /// Updates the current dialog index and checks for highlighting conditions.
+    /// </summary>
+    /// <param name="index">The new dialog index.</param>
+    private void UpdateCurrentDialogIndex(int index)
     {
         currentDialogIndex = index;
-        if (index > 35)
+        HighlightCardOnDialogChange(index);
+    }
+    
+    /// <summary>
+    /// Highlights specific cards based on the current dialog index.
+    /// </summary>
+    /// <param name="index">The current dialog index.</param>
+    private void HighlightCardOnDialogChange(int index)
+    {
+        if (index > StartHighlightMusiqueDeMegadriveIndex)
         {
             var card = CreatedCards.Find(card => card.GetComponent<CardDisplay>().Card.Title == CardNameMappings.CardNameMap[CardNames.MusiqueDeMegaDrive]);
             if (card != null)
@@ -68,7 +83,18 @@ public class TutoHandCardDisplay : HandCardDisplay
             }
         }
     }
-    
+
+    /// <summary>
+    /// Determines if a specific in-game card should be highlighted.
+    /// </summary>
+    /// <param name="handCard">The in-game card to check.</param>
+    /// <returns>True if the card should be highlighted, false otherwise.</returns>
+    private bool ShouldHighlightCard(InGameCard handCard)
+    {
+        return handCard.Title == CardNameMappings.CardNameMap[CardNames.ClichéRaciste] ||
+               (handCard.Title == CardNameMappings.CardNameMap[CardNames.MusiqueDeMegaDrive] && currentDialogIndex > StartHighlightMusiqueDeMegadriveIndex);
+    }
+
     /// <summary>
     /// Creates visual representations for the provided cards.
     /// </summary>
@@ -82,12 +108,7 @@ public class TutoHandCardDisplay : HandCardDisplay
             newCard.GetComponent<CardDisplay>().InGameCard = handCard;
             newCard.GetComponent<OnHover>().bIsInGame = true;
 
-            if (handCard.Title == CardNameMappings.CardNameMap[CardNames.ClichéRaciste])
-            {
-                newCard.AddComponent<HighLightCard>();
-            }
-            
-            if (handCard.Title == CardNameMappings.CardNameMap[CardNames.MusiqueDeMegaDrive] && currentDialogIndex > 35)
+            if (ShouldHighlightCard(handCard))
             {
                 newCard.AddComponent<HighLightCard>();
             }
@@ -97,22 +118,36 @@ public class TutoHandCardDisplay : HandCardDisplay
 
         AdjustRectTransformSize(handCards.Count);
     }
-    
+
     /// <summary>
-    /// Clears and then creates visual representations for the provided cards.
+    /// Clears any existing card representations and then recreates them for the provided cards.
     /// </summary>
     /// <param name="handCards">Collection of in-game cards.</param>
-    private void BuildCards(ObservableCollection<InGameCard> handCards)
+    private void RebuildHandDisplay(ObservableCollection<InGameCard> handCards)
     {
         ClearCreatedCards();
         CreateCards(handCards);
     }
 
+    /// <summary>
+    /// Determines if the hand cards should be displayed.
+    /// </summary>
+    /// <param name="handCards">Collection of in-game cards.</param>
+    /// <returns>True if the cards should be displayed, false otherwise.</returns>
+    private bool ShouldDisplayHandCard(ObservableCollection<InGameCard> handCards)
+    {
+        return handCards.Count == 0 || IsCurrentPlayerTurn(handCards[0]);
+    }
+
+    /// <summary>
+    /// Displays the hand cards based on certain conditions.
+    /// </summary>
+    /// <param name="handCards">Collection of in-game cards.</param>
     private void DisplayHandCard(ObservableCollection<InGameCard> handCards)
     {
-        if (handCards.Count == 0 || IsCurrentPlayerTurn(handCards[0]))
+        if (ShouldDisplayHandCard(handCards))
         {
-            BuildCards(handCards);
+            RebuildHandDisplay(handCards);
         }
     }
 }
